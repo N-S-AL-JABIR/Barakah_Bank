8b9b897c8f0e3588b54d2b4115abd19c25171c31
N-S-AL-JABIR <n.s.al.jabir@gmail.com>
2025-06-28 16:14:31 +0600

    update static

--- Barakah_Bank/settings.py
+++ Barakah_Bank/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ
 import dj_database_url
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -145,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
